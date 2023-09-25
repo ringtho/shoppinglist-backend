8cc110fcaebe69e6aae3250fae3b7c3cf9a29d42
CDiala <dialachibuzo@yahoo.com>
2023-09-25 21:18:21 +0100

    feat: Register UserAdmin model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'is_active',
+        'is_superuser'
+    )
+
+
+admin.site.register(models.User, UserAdmin)
