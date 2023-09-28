0eb614c4dfde4c9c384b1cb393b52c17c6a94494
CDiala <dialachibuzo@yahoo.com>
2023-09-28 16:24:36 +0100

    fix: Modify settings

--- shoppingAPI/settings.py
+++ shoppingAPI/settings.py
@@ -106,7 +106,6 @@ DATABASES = {
         # default='postgresql://postgres:postgres@localhost:5432/mysite',
         default='postgresql://postgres:postgres@localhost:5432/shoppingAPI',
         conn_max_age=600,
-        conn_health_checks=True,
     )
 
 }
