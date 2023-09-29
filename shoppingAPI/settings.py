2a4d382b43999c1c4719a748d72b44514ab3579d
CDiala <dialachibuzo@yahoo.com>
2023-09-29 10:08:56 +0100

    feat: Add endpoint domain to allowed origin

--- shoppingAPI/settings.py
+++ shoppingAPI/settings.py
@@ -69,7 +69,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://domain.com",
     "https://api.domain.com",
     "http://localhost:8080",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
+    "https://shoppingapi-d0lf.onrender.com/"
 ]
 
 ROOT_URLCONF = 'shoppingAPI.urls'
