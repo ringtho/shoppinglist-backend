955628ff575fabd4ab8e01b370b88f1bd61c5ed4
CDiala <dialachibuzo@yahoo.com>
2023-09-29 12:55:31 +0100

    fix: Relax auth control in orders endpoint

--- store_items/services.py
+++ store_items/services.py
@@ -60,7 +60,7 @@ def create_order(user, order: "OrderDataClass") -> "OrderDataClass":
 
 def get_orders(user) -> list["OrderDataClass"]:
     # filtered_orders = order_models.Order.objects.filter(user=user)
-    filtered_orders = order_models.Order.objects
+    filtered_orders = order_models.Order.objects.all()
 
     orders_list = [OrderDataClass.from_instance(
         order_model=order) for order in filtered_orders]
